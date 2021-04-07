af323c0d1d11ec2c340dd8255c9bb533b43aa29b
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-04-07 15:21:56 +0800

    Meta tags for 404 page

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -8,6 +8,61 @@
 
 <script>
 export default {
-  name: 'Error404'
+  name: 'Error404',
+  meta () {
+    return {
+      title: '404 | TECHMANIA',
+      meta: {
+        title: {
+          name: 'title',
+          content: '404 | TECHMANIA'
+        },
+        description: {
+          name: 'description',
+          content: 'Oops. Nothing here...'
+        },
+        ogType: {
+          name: 'og:type',
+          content: 'website'
+        },
+        ogUrl: {
+          name: 'og:url',
+          content: new URL(process.env.HOST_URL + this.$route.fullPath)
+        },
+        ogTitle: {
+          name: 'og:title',
+          content: '404 | TECHMANIA'
+        },
+        ogDescription: {
+          name: 'og:description',
+          content: 'Oops. Nothing here...'
+        },
+        ogImage: {
+          name: 'og:image',
+          content: 'https://raw.githubusercontent.com/techmania-team/techmania-team.github.io/master/public/assets/Logo_black.png'
+        },
+        twCard: {
+          name: 'twitter:card',
+          content: 'summary_large_image'
+        },
+        twUrl: {
+          name: 'twitter:url',
+          content: new URL(process.env.HOST_URL + this.$route.fullPath)
+        },
+        twTitle: {
+          name: 'twitter:title',
+          content: '404 | TECHMANIA'
+        },
+        twDescription: {
+          name: 'twitter:description',
+          content: 'Oops. Nothing here...'
+        },
+        twImage: {
+          name: 'twitter:image',
+          content: 'https://raw.githubusercontent.com/techmania-team/techmania-team.github.io/master/public/assets/Logo_black.png'
+        }
+      }
+    }
+  }
 }
 </script>
