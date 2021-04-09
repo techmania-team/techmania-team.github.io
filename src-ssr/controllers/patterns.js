24914c8220fe25f4f3fab58b09bf94269126f5b4
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-04-09 13:21:32 +0800

    API backend: submit new pattern

---
+++ src-ssr/controllers/patterns.js
@@ -0,0 +1,62 @@
+const axios = require('axios')
+const patterns = require('../models/patterns.js')
+
+module.exports = {
+  async create (req, res) {
+    try {
+      const infoidx = req.user.accessInfo.findIndex(info => info.jwt === req.token)
+      // check in discord guild or not
+      const response = await axios.get('https://discord.com/api/users/@me/guilds', {
+        headers: { Authorization: `Bearer ${req.user.accessInfo[infoidx].discord}` }
+      })
+      const inGuild = response.data.find(guild => guild.id.toString() === process.env.DISCORD_GUILD.toString())
+      if (!inGuild) {
+        res.status(403).send({ success: false, message: 'Not in guild' })
+        return
+      } else {
+        const result = await patterns.create({
+          submitter: req.user.discord,
+          name: req.body.name,
+          composer: req.body.composer,
+          keysounded: req.body.keysounded === 1,
+          difficulties: req.body.difficulties,
+          link: req.body.link,
+          previews: req.body.previews,
+          description: req.body.description
+        })
+        let strPreveiw = ''
+        for (const preview of req.body.previews) {
+          strPreveiw += `https://www.youtube.com/watch?v=${preview.ytid}\n`
+        }
+        const controls = ['Touch', 'Key', 'KM']
+        let strDifficulty = ''
+        for (const difficulty of req.body.difficulties) {
+          strDifficulty += `${controls[difficulty.control]} - ${difficulty.name}: lv.${difficulty.level}\n`
+        }
+        await axios.post(process.env.DISCORD_WEBHOOK, {
+          username: 'TECHMANIA',
+          avatar_url: 'https://avatars.githubusercontent.com/u/77661148?s=200&v=4',
+          content: `New pattern submitted by <@${req.user.discord}>`,
+          embeds: [{
+            url: new URL(`/pattern/${result._id}`, process.env.HOST_URL).toString(),
+            image: { url: `http://i3.ytimg.com/vi/${req.body.previews[0].ytid}/hqdefault.jpg` },
+            title: req.body.name,
+            color: '15158332',
+            fields: [
+              { name: 'Composer', value: req.body.composer, inline: true },
+              { name: 'Keysounded', value: req.body.keysounded === 1 ? 'Yes' : 'No', inline: true },
+              { name: 'Previews', value: strPreveiw, inline: false },
+              { name: 'Difficulties', value: strDifficulty, inline: false },
+              { name: 'Download', value: req.body.link, inline: false },
+              { name: 'Description', value: req.body.description, inline: false }
+            ]
+          }]
+        })
+        res.status(200).send({ success: true, message: '' })
+      }
+    } catch (error) {
+      console.log(error)
+      res.status(500).send({ success: true, message: 'Server Error' })
+    }
+  }
+}
--- src-ssr/routes/patterns.js
+++ src-ssr/routes/patterns.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const { create } = require('../controllers/patterns.js')
+const auth = require('../middleware/auth')
 const router = express.Router()
+
+router.post('/', auth, create)
 
 module.exports = router
