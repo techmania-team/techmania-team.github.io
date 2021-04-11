267d45c2a56cc283e466743cfbb6aee1e359faa0
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-04-11 10:46:07 +0800

    Fix default meta tags in router config

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,10 +4,10 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue'), meta: { title: 'TECHMANIA', login: false } },
-      { path: 'changelog', component: () => import('pages/Changelog.vue'), meta: { title: 'TECHMANIA | Changelog', login: false } },
-      { path: 'patterns', component: () => import('pages/Patterns.vue'), meta: { title: 'TECHMANIA | Patterns', login: false } },
+      { path: 'changelog', component: () => import('pages/Changelog.vue'), meta: { title: 'Changelog | TECHMANIA', login: false } },
+      { path: 'patterns', component: () => import('pages/Patterns.vue'), meta: { title: 'Patterns | TECHMANIA', login: false } },
       { path: 'patterns/:id', component: () => import('pages/Pattern.vue'), meta: { title: 'TECHMANIA', login: false } },
-      { path: 'mypage', component: () => import('pages/MyPage.vue'), meta: { title: 'TECHMANIA | My Page', login: true } }
+      { path: 'mypage', component: () => import('pages/MyPage.vue'), meta: { title: 'My Page | TECHMANIA', login: true } }
     ]
   },
 
