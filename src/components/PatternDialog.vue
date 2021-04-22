2510009dadea150a587225e1a0d53cd7c37ea0cb
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-04-22 22:50:16 +0800

    i18n - PatternDialog

--- src/components/PatternDialog.vue
+++ src/components/PatternDialog.vue
@@ -4,57 +4,57 @@
       q-card(style="width: 700px; max-width: 80vw;")
         q-form(@submit.prevent="submitForm")
           q-card-section
-            .text-h6 Submit New Pattern
+            .text-h6 {{ model._id && model._id.length === 0 ? $t('submitForm.title') : $t('submitForm.editTitle') }}
           q-card-section
             q-banner.text-white.bg-red(inline-actions)
-              | Please read the rules before you submit your pattern.
+              | {{ $t('submitForm.rulesTitle') }}
               ul
-                li Patterns must be for TECHMANIA only. We will delete any submissions that are for other rhythm games.
-                li Patterns made for/from DJMAX and DJMAX Technika Original tracks are not allowed to be posted.
-                li Pattens must be your own. Please do not upload other peoples work.
+                li {{ $t('submitForm.rules1') }}
+                li {{ $t('submitForm.rules2') }}
+                li {{ $t('submitForm.rules3') }}
           q-card-section
-            p.q-mb-none Song Name
-            q-input.q-mb-md(v-model="model.name" dense :rules="[val => !!val || 'Field is required']")
-            p.q-mb-none Composer
-            q-input.q-mb-md(v-model="model.composer" dense :rules="[val => !!val || 'Field is required']")
-            p.q-mb-none Download Link
-            q-input.q-mb-md(v-model="model.link" dense type="url" :rules="[val => !!val || 'Field is required']")
-            q-toggle.q-mb-md(v-model="model.keysounded" label="Keysound" left-label)
-            p.q-mb-none YouTube Preview
+            p.q-mb-none {{ $t('submitForm.songName') }}
+            q-input.q-mb-md(v-model="model.name" dense :rules="[val => !!val || $t('submitForm.required')]")
+            p.q-mb-none {{ $t('submitForm.composer') }}
+            q-input.q-mb-md(v-model="model.composer" dense :rules="[val => !!val || $t('submitForm.required')]")
+            p.q-mb-none {{ $t('submitForm.dlLink') }}
+            q-input.q-mb-md(v-model="model.link" dense type="url" :rules="[val => !!val || $t('submitForm.required')]")
+            q-toggle.q-mb-md(v-model="model.keysounded" :label="$t('pattern.keysounded')" left-label)
+            p.q-mb-none {{ $t('submitForm.preview') }}
               .q-mb-md
                 .row.items-start(v-for="(preview, index) in model.previews" :key="'A'+index")
-                  q-input.col-5(v-model="preview.name" placeholder="Name" :rules="[val => !!val || 'Field is required']")
+                  q-input.col-5(v-model="preview.name" :placeholder="$t('submitForm.name')" :rules="[val => !!val || $t('submitForm.required')]")
                   .col-1
-                  q-input.col-5(v-model="preview.link" placeholder="YouTube Link" :rules="[val => ValidYouTubeLink(val) || 'Please enter a valid URL']")
+                  q-input.col-5(v-model="preview.link" :placeholder="$t('submitForm.ytLink')" :rules="[val => ValidYouTubeLink(val) || $t('submitForm.invalidLink')]")
                   .col-1.text-center
                     q-btn(flat round icon="clear" v-if="index !== 0" @click="removePreview(index)")
                     q-btn(flat round icon="add" v-else @click="addPreview")
-            p.q-mb-none Difficulties
+            p.q-mb-none {{ $t('submitForm.difficulties') }}
               .q-mb-md
                 .row.items-start(v-for="(difficulty, index) in model.difficulties" :key="'B'+index")
-                  q-select.col-3(v-model="difficulty.control" :options="controlTypes" placeholder="Control" emit-value map-options)
+                  q-select.col-3(v-model="difficulty.control" :options="controlTypes" :placeholder="$t('submitForm.control')" emit-value map-options)
                   .col-1
-                  q-input.col-3(v-model="difficulty.name" placeholder="Name" :rules="[val => !!val || 'Field is required']")
+                  q-input.col-3(v-model="difficulty.name" :placeholder="$t('submitForm.name')" :rules="[val => !!val || $t('submitForm.required')]")
                   .col-1
-                  q-input.col-3(v-model.number="difficulty.level" type="number" placeholder="Level" :rules="[val => !!val && val > 0|| 'Field is required']")
+                  q-input.col-3(v-model.number="difficulty.level" type="number" :placeholder="$t('submitForm.level')" :rules="[val => !!val && val > 0 || $t('submitForm.required')]")
                   .col-1
                     q-btn(flat round icon="clear" v-if="index !== 0" @click="removeDifficulty(index)")
                     q-btn(flat round icon="add" v-else @click="addDifficulty")
-            p.mb-none Description
+            p.mb-none {{ $t('submitForm.description') }}
               q-input.q-mb-md(v-model="model.description" type="textarea" autogrow)
           q-card-section(v-if="model._id")
-            p.text-red DANGER ZONE
-            q-btn(color="red" @click="deleteConfirm") DELETE THIS PATTERN
+            p.text-red {{ $t('submitForm.dangerZone') }}
+            q-btn(color="red" @click="deleteConfirm") {{ $t('submitForm.delete') }}
           q-card-actions(align="right")
-            q-btn(flat label="Submit" color="tech" type="submit" :loading="submitting")
+            q-btn(flat :label="$t('submitForm.submit')" color="tech" type="submit" :loading="submitting")
     q-dialog(v-model="confirm")
       q-card
         q-card-section.row.items-center
           q-avatar.q-mx-auto(icon="warning" text-color="red")
-          span.q-ml-sm DO YOU REALLY WANT TO DELETE THIS PATTERN?
+          span.q-ml-sm {{ $t('submitForm.deleteText') }}
         q-card-actions(align="right")
-          q-btn(color="green" flat label="YES, DELETE IT" @click="deletePattern" :loading="deleting")
-          q-btn(color="red" flat label="NO, CANCEL" v-close-popup)
+          q-btn(color="green" flat :label="$t('submitForm.deleteYes')" @click="deletePattern" :loading="deleting")
+          q-btn(color="red" flat :label="$t('submitForm.deleteNo')" v-close-popup)
 </template>
 
 <script>
@@ -74,11 +74,6 @@ export default {
         previews: [{ link: '', name: '' }],
         description: ''
       },
-      controlTypes: [
-        { label: 'Touch', value: 0 },
-        { label: 'Keys', value: 1 },
-        { label: 'KM', value: 2 }
-      ],
       isedit: false,
       confirm: false
     }
@@ -95,6 +90,13 @@ export default {
       set (value) {
         this.$emit('model', value)
       }
+    },
+    controlTypes () {
+      return [
+        { label: this.$t('pattern.' + this.controls[0]), value: 0 },
+        { label: this.$t('pattern.' + this.controls[1]), value: 1 },
+        { label: this.$t('pattern.' + this.controls[2]), value: 2 }
+      ]
     }
   },
   watch: {
@@ -167,11 +169,11 @@ export default {
           throw new Error('Server Error')
         }
       } catch (error) {
-        let message = 'Server Error'
+        let message = this.$t('submitForm.errorServer')
         if (error.response.data.message === 'Not in guild') {
-          message = 'You must join our discord to submit new pattern.'
+          message = this.$t('submitForm.errorGuild')
         } else if (error.response.data.message === 'Unauthorized') {
-          message = 'Unauthorized.'
+          message = this.$t('submitForm.errorUnauthorized')
         }
         this.$q.notify({
           icon: 'warning',
@@ -239,11 +241,11 @@ export default {
           throw new Error('Server Error')
         }
       } catch (error) {
-        let message = 'Server Error'
+        let message = this.$t('submitForm.errorServer')
         if (error.response.data.message === 'Not in guild') {
-          message = 'You must join our discord to submit new pattern.'
+          message = this.$t('submitForm.errorGuild')
         } else if (error.response.data.message === 'Unauthorized') {
-          message = 'Unauthorized.'
+          message = this.$t('submitForm.errorUnauthorized')
         }
         this.$q.notify({
           icon: 'warning',
