ec80e5e8c00ab8fa2ed0647f99f5a83d812e05a9
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-04-24 20:44:09 +0800

    Fix relative time locale

--- src/components/PatternCard.vue
+++ src/components/PatternCard.vue
@@ -57,14 +57,14 @@ export default {
   computed: {
     formattedTime () {
       return {
-        relative: this.$date(this.pattern.submitDate).locale(this.$q.lang.getLocale()).fromNow(),
-        text: new Date(this.pattern.submitDate).toLocaleString(this.$q.lang.getLocale())
+        relative: this.$date(this.pattern.submitDate).locale(this.user.locale).fromNow(),
+        text: new Date(this.pattern.submitDate).toLocaleString(this.user.locale)
       }
     },
     formattedUpdateTime () {
       return {
-        relative: this.$date(this.pattern.updateDate).locale(this.$q.lang.getLocale()).fromNow(),
-        text: new Date(this.pattern.updateDate).toLocaleString(this.$q.lang.getLocale())
+        relative: this.$date(this.pattern.updateDate).locale(this.user.locale).fromNow(),
+        text: new Date(this.pattern.updateDate).toLocaleString(this.user.locale)
       }
     }
   }
