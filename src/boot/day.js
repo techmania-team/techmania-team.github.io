abc9c3ec171671489107086d064d042343ff0745
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-04-25 19:00:47 +0800

    Add ja-jp and ko-kr to locale select

--- src/boot/day.js
+++ src/boot/day.js
@@ -3,6 +3,8 @@ import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import 'dayjs/locale/zh-tw'
 import 'dayjs/locale/zh-cn'
+import 'dayjs/locale/ja'
+import 'dayjs/locale/ko'
 
 dayjs.extend(relativeTime)
 
--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -7,7 +7,7 @@ Vue.use(VueI18n)
 export default ({ app, store }) => {
   // Set i18n instance on app
   app.i18n = new VueI18n({
-    locale: store.getters['user/getLocale'],
+    locale: store.getters['user/getUserData'].locale,
     fallbackLocale: 'en-us',
     messages
   })
