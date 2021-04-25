aebfd6274db3e37692bc96e4f3079bd60d4f4099
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-04-25 13:00:18 +0800

    Add dayjs zh-cn locale

--- src/boot/day.js
+++ src/boot/day.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import 'dayjs/locale/zh-tw'
+import 'dayjs/locale/zh-cn'
 
 dayjs.extend(relativeTime)
 
