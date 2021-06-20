5280736b1c5577f0a968388ce8903e4e6a9a1ebf
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-06-21 01:02:43 +0800

    Add iOS download link

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,26 +8,43 @@
           img#logo(:src="'./assets/Logo.png'")
           div(v-if="platform === 'windows'")
             .text-h3
-              q-btn.q-my-md(color="secondary" size="lg" type='a' href='https://github.com/macmillan333/techmania/releases/latest')
+              q-btn.q-my-md(color="secondary" size="lg" type='a' href='https://github.com/macmillan333/techmania/releases/latest' target='_blank')
                 q-icon(left name="fab fa-windows" )
                 div {{ $t('index.download') }}
             p
-              | {{ $t('index.version') }}: {{ tag }}
+              | {{ $t('index.version') }}: {{ tag.win }}
               br
-              | {{ $t('index.release') }} {{ published }}
+              | {{ $t('index.release') }} {{ published.win }}
               br
               a(href="#" @click.prevent="platform = 'android'") {{ $t('index.platform', {platform: 'Android'}) }}
+              | &emsp;
+              a(href="#" @click.prevent="platform = 'ios'") {{ $t('index.platform', {platform: 'iOS'}) }}
           div(v-else-if="platform === 'android'")
             .text-h3
-              q-btn.q-my-md(color="secondary" size="lg" type='a' href='https://drive.google.com/file/d/11jgs4E46cm6swlt6CN4j7kkwjljSiDdj/view')
+              q-btn.q-my-md(color="secondary" size="lg" type='a' href='https://drive.google.com/file/d/11jgs4E46cm6swlt6CN4j7kkwjljSiDdj/view' target='_blank')
                 q-icon(left name="android" )
                 div {{ $t('index.download') }}
             p
-              | {{ $t('index.version') }}: {{ tagAndroid }}
+              | {{ $t('index.version') }}: {{ tag.android }}
               br
-              | {{ $t('index.release') }} {{ publishAndroid }}
+              | {{ $t('index.release') }} {{ published.android }}
               br
               a(href="#" @click.prevent="platform = 'windows'") {{ $t('index.platform', {platform: 'Windows'}) }}
+              | &emsp;
+              a(href="#" @click.prevent="platform = 'ios'") {{ $t('index.platform', {platform: 'iOS'}) }}
+          div(v-else-if="platform === 'ios'")
+            .text-h3
+              q-btn.q-my-md(color="secondary" size="lg" type='a' href='https://github.com/rogeraabbccdd/techmania/releases' target='_blank')
+                q-icon(left name="fab fa-apple" )
+                div {{ $t('index.download') }}
+            p
+              | {{ $t('index.version') }}: {{ tag.ios }}
+              br
+              | {{ $t('index.release') }} {{ published.ios }}
+              br
+              a(href="#" @click.prevent="platform = 'windows'") {{ $t('index.platform', {platform: 'Windows'}) }}
+              | &emsp;
+              a(href="#" @click.prevent="platform = 'android'") {{ $t('index.platform', {platform: 'Android'}) }}
     section.q-mx-auto.padding.q-my-md
       Patterns#index-patterns
     section.q-mx-auto.padding.q-my-md
@@ -101,23 +118,29 @@ export default {
   },
   data () {
     return {
-      tag: '',
-      tagAndroid: '0.7',
+      tag: {
+        win: '',
+        ios: '0.7',
+        android: '0.7'
+      },
       publishDate: '',
-      publishAndroid: '',
       platform: 'windows'
     }
   },
   computed: {
     published () {
-      return this.publishDate.length > 0 ? new Date(this.publishDate).toLocaleString(this.user.locale) : 'Unknown'
+      return {
+        win: this.publishDate.length > 0 ? new Date(this.publishDate).toLocaleString(this.user.locale) : 'Unknown',
+        android: new Date('2021/06/14 14:33:00 GMT+0800').toLocaleString(this.user.locale),
+        ios: new Date('2021/06/20 21:45:00 GMT+0800').toLocaleString(this.user.locale)
+      }
     }
   },
   methods: {
     async getLatestTag () {
       try {
         const result = await this.$axios.get('https://api.github.com/repos/techmania-team/techmania/releases')
-        this.tag = result.data[0].tag_name
+        this.tag.win = result.data[0].tag_name
         this.publishDate = result.data[0].published_at
       } catch (_) {
         this.tag = 'Unknown'
@@ -126,8 +149,8 @@ export default {
   },
   mounted () {
     this.getLatestTag()
-    this.publishAndroid = new Date('2021/06/14 14:33:00 GMT+0800').toLocaleString(this.user.locale)
     if (this.$q.platform.is.android) this.platform = 'android'
+    else if (this.$q.platform.is.ios) this.platform = 'ios'
   }
 }
 </script>
