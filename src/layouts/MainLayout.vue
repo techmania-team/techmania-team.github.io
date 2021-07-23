7b4d4d01995ba3ad506eec806ca1f9a4dbfe4788
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-07-23 16:46:31 +0800

    Fix hydrate error

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,47 +1,48 @@
 <template lang="pug">
   q-layout(view='hHh lpR fff')
     q-header.bg-techgrey.text-white(reveal elevated)
-      .container
-        q-toolbar
-          q-toolbar-title
-            q-btn(to="/" flat)
-              q-avatar
-                img(:src="'./assets/notes/basic.png'")
-              | &nbsp;TECHMANIA
-          q-tabs
-            q-tab.nav-desktop(@click="openLink('https://techmania-team.github.io/techmania-docs/', '_blank')" :label="$t('nav.manual')")
-            q-route-tab.nav-desktop(v-for="(nav, idx) in navs" :key="idx" :to="nav.link" :label="$t(nav.label)")
-            q-tab.nav-desktop(v-if="!isLogin" @click="openLink(discordURL.login, '_self')" :label="$t('nav.login')")
-            q-route-tab.nav-desktop(v-if="isLogin" to="/mypage" :label="$t('nav.myPage')")
-            q-tab.nav-desktop(v-if="isLogin" @click="logout()" :label="$t('nav.logout')")
-            q-btn-dropdown.nav-desktop(stretch flat :label="$t('nav.lang')")
-              q-list
-                q-item(clickable v-close-popup v-for="(locale, lid) in localeOptions" :key="lid" @click="updateLocale(locale)")
-                  q-item-section
-                    q-item-label {{ locale.toUpperCase() }}
-            q-btn(round v-if="isLogin" to="/mypage")
-              q-avatar
-                img(:src="user.avatar_url")
-            q-btn.nav-mobile(:label="isLogin ? '' : $t('nav.menu')" :icon-right="dropdown ? 'keyboard_arrow_up' : 'keyboard_arrow_down'" @click="dropdown = !dropdown")
-        q-separator.nav-mobile(v-show="dropdown")
-      q-slide-transition
-        .container.nav-mobile(v-show="dropdown")
-          q-list
-            q-item.text-grey7(clickable v-if="!isLogin" @click="openLink('https://techmania-team.github.io/techmania-docs/', '_blank'); dropdown = !dropdown" active-class="text-white")
-              q-item-section {{ $t('nav.manual') }}
-            q-item.text-grey7(clickable @click="dropdown = !dropdown" v-for="(nav, idx) in navs" :key="idx" :to="nav.link" active-class="text-white")
-              q-item-section {{ $t(nav.label) }}
-            q-item.text-grey7(clickable v-if="!isLogin" @click="openLink(discordURL.login, '_self'); dropdown = !dropdown" active-class="text-white")
-              q-item-section {{ $t('nav.login') }}
-            q-item.text-grey7(clickable @click="dropdown = !dropdown" v-if="isLogin" to="/mypage" active-class="text-white")
-              q-item-section {{ $t('nav.myPage') }}
-            q-item.text-grey7(clickable v-if="isLogin" @click="logout()" active-class="text-white")
-              q-item-section {{ $t('nav.logout') }}
-            q-btn-dropdown.full-width(align="between" stretch flat :label="$t('nav.lang')")
-              q-list
-                q-item(clickable v-close-popup v-for="(locale, lid) in localeOptions" :key="lid" @click="updateLocale(locale)")
-                  q-item-section
-                    q-item-label {{ locale.toUpperCase() }}
+      q-no-ssr
+        .container
+          q-toolbar
+            q-toolbar-title
+              q-btn(to="/" flat)
+                q-avatar
+                  img(:src="'./assets/notes/basic.png'")
+                | &nbsp;TECHMANIA
+            q-tabs
+              q-tab.nav-desktop(@click="openLink('https://techmania-team.github.io/techmania-docs/', '_blank')" :label="$t('nav.manual')")
+              q-route-tab.nav-desktop(v-for="(nav, idx) in navs" :key="idx" :to="nav.link" :label="$t(nav.label)")
+              q-tab.nav-desktop(v-if="!isLogin" @click="openLink(discordURL.login, '_self')" :label="$t('nav.login')")
+              q-route-tab.nav-desktop(v-if="isLogin" to="/mypage" :label="$t('nav.myPage')")
+              q-tab.nav-desktop(v-if="isLogin" @click="logout()" :label="$t('nav.logout')")
+              q-btn-dropdown.nav-desktop(stretch flat :label="$t('nav.lang')")
+                q-list
+                  q-item(clickable v-close-popup v-for="(locale, lid) in localeOptions" :key="lid" @click="updateLocale(locale)")
+                    q-item-section
+                      q-item-label {{ locale.toUpperCase() }}
+              q-btn(round v-if="isLogin" to="/mypage")
+                q-avatar
+                  img(:src="user.avatar_url")
+              q-btn.nav-mobile(:label="isLogin ? '' : $t('nav.menu')" :icon-right="dropdown ? 'keyboard_arrow_up' : 'keyboard_arrow_down'" @click="dropdown = !dropdown")
+          q-separator.nav-mobile(v-show="dropdown")
+        q-slide-transition
+          .container.nav-mobile(v-show="dropdown")
+            q-list
+              q-item.text-grey7(clickable v-if="!isLogin" @click="openLink('https://techmania-team.github.io/techmania-docs/', '_blank'); dropdown = !dropdown" active-class="text-white")
+                q-item-section {{ $t('nav.manual') }}
+              q-item.text-grey7(clickable @click="dropdown = !dropdown" v-for="(nav, idx) in navs" :key="idx" :to="nav.link" active-class="text-white")
+                q-item-section {{ $t(nav.label) }}
+              q-item.text-grey7(clickable v-if="!isLogin" @click="openLink(discordURL.login, '_self'); dropdown = !dropdown" active-class="text-white")
+                q-item-section {{ $t('nav.login') }}
+              q-item.text-grey7(clickable @click="dropdown = !dropdown" v-if="isLogin" to="/mypage" active-class="text-white")
+                q-item-section {{ $t('nav.myPage') }}
+              q-item.text-grey7(clickable v-if="isLogin" @click="logout()" active-class="text-white")
+                q-item-section {{ $t('nav.logout') }}
+              q-btn-dropdown.full-width(align="between" stretch flat :label="$t('nav.lang')")
+                q-list
+                  q-item(clickable v-close-popup v-for="(locale, lid) in localeOptions" :key="lid" @click="updateLocale(locale)")
+                    q-item-section
+                      q-item-label {{ locale.toUpperCase() }}
     q-page-container
       router-view.q-mb-xl(:key="$route.fullPath")
     q-footer.bg-techgrey.text-white.relative-position(bordered)
