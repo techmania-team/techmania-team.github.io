80ba8d2e25c6583244d0a345bf8239dc60808b18
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-07-23 16:46:46 +0800

    Move some functions out from mixin

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,40 @@
 <script>
 export default {
   name: 'App',
+  methods: {
+    async login () {
+      if (this.$route.query.jwt && this.$route.query.token) {
+        this.$store.commit('user/addjwt', this.$route.query.jwt)
+        this.$store.commit('user/addtoken', this.$route.query.token)
+        this.$store.commit('user/addid', this.$route.query.id)
+        this.$router.replace({ query: {} })
+      }
+      if (this.user.jwt.length > 0) {
+        // 5 days = 432000000 ms
+        if (Date.now() - this.user.jwtReceived > 432000000) {
+          await this.extendToken()
+          return
+        }
+        this.getUserData(this.user.token)
+      }
+    },
+    async getUserData (token) {
+      try {
+        const result = await this.$axios.get(this.discordURL.identity,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          }
+        )
+        this.$store.commit('user/login', { ...result.data })
+      } catch (error) {
+        this.$store.commit('user/logout')
+      }
+    }
+  },
   async created () {
-    this.login()
-
+    await this.login()
     if (this.user.locale) {
       this.$i18n.locale = this.user.locale
     } else {
--- src/boot/mixin.js
+++ src/boot/mixin.js
@@ -52,20 +52,6 @@ export default async ({ Vue }) => {
         }
         return icon
       },
-      async getUserData (token) {
-        try {
-          const result = await this.$axios.get(this.discordURL.identity,
-            {
-              headers: {
-                Authorization: `Bearer ${token}`
-              }
-            }
-          )
-          this.$store.commit('user/login', { ...result.data })
-        } catch (error) {
-          this.$store.commit('user/logout')
-        }
-      },
       async extendToken () {
         if (this.user.jwt.length > 0) {
           try {
@@ -79,22 +65,6 @@ export default async ({ Vue }) => {
           } catch (error) {
             this.$store.commit('user/logout')
           }
-        }
-      },
-      async login () {
-        if (this.$route.query.jwt && this.$route.query.token) {
-          this.$store.commit('user/addjwt', this.$route.query.jwt)
-          this.$store.commit('user/addtoken', this.$route.query.token)
-          this.$store.commit('user/addid', this.$route.query.id)
-          this.$router.replace({ query: {} })
-        }
-        if (this.user.jwt.length > 0) {
-          // 5 days = 432000000 ms
-          if (Date.now() - this.user.jwtReceived > 432000000) {
-            await this.extendToken()
-            return
-          }
-          this.getUserData(this.user.token)
         }
       },
       async logout () {
