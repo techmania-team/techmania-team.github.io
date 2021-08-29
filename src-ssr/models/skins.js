c34a933766e93202162388980fd700ede28a39d5
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-08-29 09:54:00 +0800

    Add skins schema

---
+++ src-ssr/models/skins.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+const previewSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  ytid: {
+    type: String,
+    required: true
+  }
+}, { versionKey: false })
+
+const schema = new mongoose.Schema({
+  submitter: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  // 0 = Note
+  // 1 = VFX
+  // 2 = Combo
+  // 3 = Game UI
+  type: {
+    type: Number,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  previews: {
+    type: [previewSchema],
+    required: true
+  },
+  description: {
+    type: String
+  },
+  submitDate: {
+    type: Date,
+    default: Date.now
+  },
+  updateDate: {
+    type: Date,
+    default: Date.now
+  }
+}, { versionKey: false })
+
+module.exports = mongoose.model('skins', schema)
