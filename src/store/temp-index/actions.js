5500a98302495c995ea0e36c8de33cfdd59a2f93
rogeraabbccdd <rogeraabbccdd@gmail.com>
2022-01-28 17:30:25 +0800

    Rewrite index data fetch function

--- src/store/temp-index/actions.js
+++ src/store/temp-index/actions.js
@@ -1,29 +1,25 @@
 export async function fetchData ({ commit }) {
   try {
-    let pt = this._vm.$api.get(new URL('/api/patterns?start=0&limit=8', process.env.HOST_URL).toString())
-    let skin = this._vm.$api.get(new URL('/api/skins?start=0&limit=8', process.env.HOST_URL).toString())
-    let vid = this._vm.$api.get(new URL('/api/patterns/indexvideo', process.env.HOST_URL).toString())
-    pt = await pt
-    skin = await skin
-    vid = await vid
+    const [pt, skin, vid] = await Promise.all([
+      this._vm.$api.get(new URL('/api/patterns?start=0&limit=8', process.env.HOST_URL).toString()),
+      this._vm.$api.get(new URL('/api/skins?start=0&limit=8', process.env.HOST_URL).toString()),
+      this._vm.$api.get(new URL('/api/patterns/indexvideo', process.env.HOST_URL).toString())
+    ])
     commit('setPt', pt.data.result)
     commit('setSkin', skin.data.result)
-    vid = vid.data.result.map(v => 'https://www.youtube.com/embed/' + v.ytid)
-    commit('setVid', vid)
+    commit('setVid', vid.data.result.map(v => 'https://www.youtube.com/embed/' + v.ytid))
   } catch (_) {
   }
 }
 
 export async function fetchGitHub ({ commit }) {
   try {
-    let win = this._vm.$axios.get('https://api.github.com/repos/techmania-team/techmania/releases')
-    let ios = this._vm.$axios.get('https://api.github.com/repos/rogeraabbccdd/techmania/releases')
-    let mac = this._vm.$axios.get('https://api.github.com/repos/fhalfkg/techmania/releases')
-    let and = this._vm.$axios.get('https://api.github.com/repos/rogeraabbccdd/techmania/releases')
-    win = await win
-    ios = await ios
-    mac = await mac
-    and = await and
+    const [win, ios, mac, and] = await Promise.all([
+      this._vm.$axios.get('https://api.github.com/repos/techmania-team/techmania/releases'),
+      this._vm.$axios.get('https://api.github.com/repos/rogeraabbccdd/techmania/releases'),
+      this._vm.$axios.get('https://api.github.com/repos/fhalfkg/techmania/releases'),
+      this._vm.$axios.get('https://api.github.com/repos/rogeraabbccdd/techmania/releases')
+    ])
     commit('setTag', { win: win.data[0].tag_name, ios: ios.data[0].tag_name, mac: mac.data[0].tag_name, android: and.data[0].tag_name })
     commit('setPublish', { win: win.data[0].published_at, ios: ios.data[0].published_at, mac: mac.data[0].published_at, android: and.data[0].published_at })
   } catch (_) {
