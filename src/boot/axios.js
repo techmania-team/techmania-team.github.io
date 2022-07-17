db8e9a0793e5429d3b467fc3a7062c7124b463f2
rogeraabbccdd <rogeraabbccdd@gmail.com>
2022-07-17 14:29:51 +0800

    Upgrade axios boot file

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,38 +1,55 @@
+import { boot } from 'quasar/wrappers'
 import axios from 'axios'
+import store from '../store'
+import router from '../router'
 
+// Be careful when using SSR for cross-request state pollution
+// due to creating a Singleton instance here;
+// If any client changes this (global) instance, it might be a
+// good idea to move this instance creation inside of the
+// "export default () => {}" function below (which runs individually
+// for each client)
 const api = axios.create({
   baseURL: new URL('/api', process.env.HOST_URL).toString()
 })
 
-export default ({ Vue, router, store }) => {
-  Vue.prototype.$axios = axios
-
-  Vue.prototype.$api = api
-
-  Vue.prototype.$api.interceptors.response.use((response) => {
-    return response
-  }, (error) => {
-    if (error.response) {
-      if (error.response.status === 401) {
-        const extendUrl = '/users/extend'
-        if (!error.config.url.href.includes(extendUrl)) {
-          const originalRequest = error.config
-          return axios.post(extendUrl, {}, {
-            headers: { Authorization: `Bearer ${store.state.user.jwt}` }
-          }).then(({ data }) => {
-            store.commit('user/addjwt', data.jwt)
-            store.commit('user/addtoken', data.token)
-            store.commit('user/addid', data.id)
-            originalRequest.headers.Authorization = 'Bearer ' + store.state.user.jwt
-            return axios(originalRequest)
-          }).catch((error) => {
-            store.commit('user/logout')
-            router.push('/')
-            return Promise.reject(error)
-          })
-        }
+api.interceptors.response.use((response) => {
+  return response
+}, (error) => {
+  if (error.response) {
+    if (error.response.status === 401) {
+      const extendUrl = '/users/extend'
+      if (!error.config.url.href.includes(extendUrl)) {
+        const originalRequest = error.config
+        return axios.post(extendUrl, {}, {
+          headers: { Authorization: `Bearer ${store.state.user.jwt}` }
+        }).then(({ data }) => {
+          store.commit('user/addjwt', data.jwt)
+          store.commit('user/addtoken', data.token)
+          store.commit('user/addid', data.id)
+          originalRequest.headers.Authorization = 'Bearer ' + store.state.user.jwt
+          return axios(originalRequest)
+        }).catch((error) => {
+          store.commit('user/logout')
+          router.push('/')
+          return Promise.reject(error)
+        })
       }
     }
-    return Promise.reject(error)
-  })
-}
+  }
+  return Promise.reject(error)
+})
+
+export default boot(({ app, router, store }) => {
+  // for use inside Vue files (Options API) through this.$axios and this.$api
+
+  app.config.globalProperties.$axios = axios
+  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
+  //       so you won't necessarily have to import axios in each vue file
+
+  app.config.globalProperties.$api = api
+  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
+  //       so you can easily perform requests against your app's API
+})
+
+export { api }
