19b65d314c12a77a47b4f3e245c779c47c4d3c5c
rogeraabbccdd <rogeraabbccdd@gmail.com>
2022-07-17 13:38:22 +0800

    Convert Stylus to SASS

--- quasar.conf.js
+++ quasar.conf.js
@@ -34,7 +34,7 @@ module.exports = configure(function (ctx) {
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: [
-      'app.styl'
+      'app.sass'
     ],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
