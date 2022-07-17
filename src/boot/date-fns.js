b04326e8bbc9cc03d92fcf7a86c21c82e025f884
rogeraabbccdd <rogeraabbccdd@gmail.com>
2022-07-17 14:29:58 +0800

    Upgrade date-fns boot file

--- src/boot/date-fns.js
+++ src/boot/date-fns.js
@@ -1,8 +1,16 @@
+import { boot } from 'quasar/wrappers'
 import { formatDistanceToNow } from 'date-fns'
 import { enUS, ja, ko, zhCN, zhTW } from 'date-fns/locale'
-import Vue from 'vue'
 
-Vue.prototype.$date = {
-  formatDistanceToNow,
-  locales: { enUS, ja, ko, zhCN, zhTW }
+const datePlugin = {
+  install: (app, options) => {
+    app.config.globalProperties.$date = {
+      formatDistanceToNow,
+      locales: { enUS, ja, ko, zhCN, zhTW }
+    }
+  }
 }
+export default boot(({ app }) => {
+  // Set i18n instance on app
+  app.use(datePlugin)
+})
