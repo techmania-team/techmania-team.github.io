6d6ce9d3b3e045fcbcc395f04c939c2b2db4852e
Kento <rogeraabbccdd@gmail.com>
2025-02-27 08:41:56 +0800

    Update Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { createStore } from 'vuex'
 import user from './user'
 import tempPattern from './temp-pattern'
 import tempSkin from './temp-skin'
 import tempIndex from './temp-index'
 import tempProfile from './temp-profile'
 
-Vue.use(Vuex)
-
-/*
- * If not building with SSR mode, you can
- * directly export the Store instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Store instance.
- */
-
-export default function ({ ssrContext }) {
-  const Store = new Vuex.Store({
+export default function (/* { ssrContext } */) {
+  const Store = createStore({
     modules: {
       user,
       tempPattern,
@@ -26,8 +14,9 @@ export default function ({ ssrContext }) {
       tempProfile,
       tempSkin
     },
+
     // enable strict mode (adds overhead!)
-    // for dev mode only
+    // for dev mode and --debug builds only
     strict: process.env.DEBUGGING
   })
 
