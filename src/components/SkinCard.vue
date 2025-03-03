a86921cb78af9ab022744ecf3283c34b227afd18
Kento <rogeraabbccdd@gmail.com>
2025-03-03 11:44:29 +0800

    Update SkinCard component to composition API

--- src/components/SkinCard.vue
+++ src/components/SkinCard.vue
@@ -1,22 +1,34 @@
 <template lang="pug">
-q-card.full-height.card-pattern
+q-card.full-height.card-skin
+  //- Header video
   q-video(v-if="video && hasVideo" :src="`https://www.youtube.com/embed/${videoLink}`" :ratio="16/9")
+  //- Header image
   q-img.cursor-pointer(v-else :src="headerImage" :ratio="16/9" @click="clickHeader")
     .absolute.full-width.full-height.flex.justify-center.items-center(v-if='hasVideo')
       h1.q-ma-none
         q-icon.text-white(name="play_circle_outline")
+  //- Content
   q-card-section
+    //- Download or edit button
     q-btn.btn-dl.absolute(v-if="!mine" fab icon="download" color="tech" text-color="black" type="a" :href="skin.link" target="__blank")
     q-btn.btn-dl.absolute(v-if="mine" fab icon="edit" color="tech" text-color="black" @click="$router.push('/skins/edit/' + skin._id)")
+    //- Informations
     q-list
+      //- Link
       q-item
         q-item-section.card-title
           router-link(:to="'/skins/' + skin._id")
             .text-h6 {{ skin.name }}
+      //- Type
       q-item
         q-item-section
-          q-rating(v-model="skin.rating.rating" readonly icon="star" icon-half="star_half" size='xs')
+          span {{ $t('submitSkinForm.skinType') }}: {{ typeName }}
+      //- Rating
+      q-item
+        q-item-section
+          q-rating(:model-value="skin.rating.rating" readonly icon="star" icon-half="star_half" size='xs')
           | {{ skin.rating.rating.toFixed(2) }} / {{ $t('pattern.ratingCount', {count: skin.rating.count}) }}
+      //- Date
       q-item
         q-item-section
           p
@@ -25,62 +37,77 @@ q-card.full-height.card-pattern
               router-link.no-underline(:to='`/users/${skin.submitter._id}/skins`') {{ skin.submitter.name }}
             br(v-if="!mine")
             span {{ $t('pattern.submitted') }} {{ formattedTime.relative }}
-              q-tooltip(anchor="top middle" self="bottom middle" content-style="background: #000")
+              q-tooltip.bg-black(anchor="top middle" self="bottom middle")
                 | {{ formattedTime.text }}
             br
             span {{ $t('pattern.updated') }} {{ formattedUpdateTime.relative }}
-              q-tooltip(anchor="top middle" self="bottom middle" content-style="background: #000")
+              q-tooltip.bg-black(anchor="top middle" self="bottom middle")
                 | {{ formattedUpdateTime.text }}
-            br
-            span {{ $t('submitSkinForm.skinType') }}: {{ typeName }}
 </template>
 
-<script>
+<script setup>
+import { ref, computed, onMounted } from 'vue'
 import parseISO from 'date-fns/fp/parseISO'
-export default {
-  name: 'SkinCard',
-  props: {
-    skin: Object,
-    mine: Boolean
-  },
-  data () {
-    return {
-      video: false,
-      videoLink: '',
-      hasVideo: false,
-      hasImage: false,
-      headerImage: ''
-    }
-  },
-  computed: {
-    formattedTime () {
-      return {
-        relative: this.$date.formatDistanceToNow(parseISO(this.skin.submitDate), { locale: this.$date.locales[this.user.locale2], addSuffix: true }),
-        text: new Date(this.skin.submitDate).toLocaleString(this.user.locale)
-      }
-    },
-    formattedUpdateTime () {
-      return {
-        relative: this.$date.formatDistanceToNow(parseISO(this.skin.updateDate), { locale: this.$date.locales[this.user.locale2], addSuffix: true }),
-        text: new Date(this.skin.updateDate).toLocaleString(this.user.locale)
-      }
-    },
-    typeName () {
-      const type = [this.$t('skin.note'), this.$t('skin.vfx'), this.$t('skin.combo'), this.$t('skin.gameUI')]
-      return type[this.skin.type]
-    }
-  },
-  methods: {
-    clickHeader () {
-      if (this.hasVideo) this.video = true
-      else this.$router.push('/skins/' + this.skin._id)
-    }
-  },
-  created () {
-    this.videoLink = this.skin.previews?.[0]?.ytid || ''
-    this.hasVideo = this.skin.previews?.[0]?.ytid !== undefined
-    this.hasImage = this.skin.image?.length > 0 || false
-    this.headerImage = this.skin.image?.length > 0 ? this.skin.image : this.skin.previews.length > 0 ? this.getYouTubeThumbnail(this.skin.previews[0].ytid) : './assets/unknown.jpg'
+import { useUserStore } from 'src/stores/user'
+import date from 'src/utils/date-fns'
+import { useRouter } from 'vue-router'
+import { getYouTubeThumbnail } from 'src/utils/youtube'
+import { useI18n } from 'vue-i18n'
+
+const props = defineProps({
+  skin: Object,
+  mine: Boolean,
+})
+
+const video = ref(false)
+const videoLink = ref('')
+const hasVideo = ref(false)
+const hasImage = ref(false)
+const headerImage = ref('')
+
+const user = useUserStore()
+const router = useRouter()
+const { t } = useI18n()
+
+const formattedTime = computed(() => {
+  return {
+    relative: date.formatDistanceToNow(parseISO(props.skin.submitDate), {
+      locale: date.locales[user.locale],
+      addSuffix: true,
+    }),
+    text: new Date(props.skin.submitDate).toLocaleString(user.locale),
   }
+})
+
+const formattedUpdateTime = computed(() => {
+  return {
+    relative: date.formatDistanceToNow(parseISO(props.skin.updateDate), {
+      locale: date.locales[user.locale],
+      addSuffix: true,
+    }),
+    text: new Date(props.skin.updateDate).toLocaleString(user.locale),
+  }
+})
+
+const typeName = computed(() => {
+  const type = [t('skin.note'), t('skin.vfx'), t('skin.combo'), t('skin.gameUI')]
+  return type[props.skin.type]
+})
+
+const clickHeader = () => {
+  if (hasVideo.value) video.value = true
+  else router.push('/skins/' + props.skin._id)
 }
+
+onMounted(() => {
+  videoLink.value = props.skin.previews?.[0]?.ytid || ''
+  hasVideo.value = props.skin.previews?.[0]?.ytid !== undefined
+  hasImage.value = props.skin.image?.length > 0 || false
+  headerImage.value =
+    props.skin.image?.length > 0
+      ? props.skin.image
+      : props.skin.previews.length > 0
+        ? getYouTubeThumbnail(props.skin.previews[0].ytid)
+        : './assets/unknown.jpg'
+})
 </script>
