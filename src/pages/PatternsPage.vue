8186b8cccc3a157a22fe68e707273af0d70fbb42
Kento <rogeraabbccdd@gmail.com>
2025-03-09 15:00:22 +0800

    Update patterns page to fit new search API and migrate to vee-validate

--- src/pages/PatternsPage.vue
+++ src/pages/PatternsPage.vue
@@ -1,64 +1,103 @@
 <template lang="pug">
 q-page#patterns
-  section.q-mx-auto.padding
+  q-no-ssr.q-mx-auto.padding
     .container
       .row
         .col-12.q-mx-auto
+          //- Page title
           h4.text-center {{ $t('nav.patterns') }}
           q-separator.q-my-md
           br
-          q-input(rounded outlined v-model="searchForm.keywords" :placeholder="$t('patterns.search')" @keydown.enter="applySearch")
-            template(#after)
-              q-btn(icon="search" round desnse flat @click="applySearch")
-          br
-          q-list.search
-            q-item
-              q-item-section {{ $t('pattern.keysounded') }}
-              q-item-section
-                div
-                  q-btn(flat size="10px" :label="$t('patterns.all')" :text-color="searchForm.keysounded === -1 ? 'white' : 'grey'" @click="searchForm.keysounded = -1")
-                  q-btn(flat size="10px" :label="$t('patterns.yes')" :text-color="searchForm.keysounded === 1 ? 'white' : 'grey'" @click="searchForm.keysounded = 1")
-                  q-btn(flat size="10px" :label="$t('patterns.no')" :text-color="searchForm.keysounded === 0 ? 'white' : 'grey'" @click="searchForm.keysounded = 0")
-            q-item
-              q-item-section {{ $t('submitForm.control') }}
-              q-item-section
-                div
-                  q-btn(flat size="10px" :label="$t('patterns.all')" :text-color="searchForm.control === '' ? 'white' : 'grey'" @click="searchForm.control = ''")
-                  q-btn(flat size="10px" :label="$t('pattern.touch')" :text-color="searchForm.control === '0' ? 'white' : 'grey'" @click="searchForm.control = '0'")
-                  q-btn(flat size="10px" :label="$t('pattern.keys')" :text-color="searchForm.control === '1' ? 'white' : 'grey'" @click="searchForm.control = '1'")
-                  q-btn(flat size="10px" :label="$t('pattern.km')" :text-color="searchForm.control === '2' ? 'white' : 'grey'" @click="searchForm.control = '2'")
-            q-item
-              q-item-section {{ $t('submitForm.lanes') }}
-              q-item-section
-                div
-                  q-option-group(inline :options="[{label: '2', value: 2}, {label: '3', value: 3}, {label: '4', value: 4}]" type="checkbox" v-model="searchForm.lanes")
-            q-item
-              q-item-section {{ $t('patterns.sort') }}
-              q-item-section
-                div
-                  q-btn(flat size="10px" :label="$t('patterns.sortSubmit')" :icon-right="getSortIcon('createdAt')" :text-color="searchForm.sortBy === 'createdAt' ? 'white' : 'grey'" @click="changeSort('createdAt')")
-                  q-btn(flat size="10px" :label="$t('patterns.sortUpdate')" :icon-right="getSortIcon('updatedAt')" :text-color="searchForm.sortBy === 'updatedAt' ? 'white' : 'grey'" @click="changeSort('updatedAt')")
-                  q-btn(flat size="10px" :label="$t('patterns.sortName')" :icon-right="getSortIcon('name')" :text-color="searchForm.sortBy === 'name' ? 'white' : 'grey'" @click="changeSort('name')")
-                  q-btn(flat size="10px" :label="$t('pattern.rating')" :icon-right="getSortIcon('rating')" :text-color="searchForm.sortBy === 'rating' ? 'white' : 'grey'" @click="changeSort('rating')")
+          //- Search form
+          Form(v-slot="{ handleSubmit, isSubmitting }" :validation-schema="schema" :initial-values="initialValues" ref="form" as="")
+            q-form(@submit="handleSubmit(onSearchSubmit)")
+              //- Keywords
+              Field(name="keywords" v-slot="{ field, errorMessage }")
+                q-input(
+                  square outlined color="tech"
+                  :model-value="field.value"
+                  @update:model-value="field.onChange($event)"
+                  @blur="field.onBlur($event)"
+                  :error-message="errorMessage"
+                  :error="!!errorMessage"
+                  :placeholder="$t('patterns.search')"
+                )
+                  template(#after)
+                    //- NOTE:
+                    //- Button type Submit in slot does not trigger the submit event
+                    //- https://quasar.dev/vue-components/button#controlling-the-button-for-form-submission
+                    q-btn(icon="search" round desnse flat @click="handleSubmit($event, onSearchSubmit)" :loading="isSubmitting")
+              //- Filters
+              q-list
+                //- Keysounded
+                q-item
+                  q-item-section {{ $t('pattern.keysounded') }}
+                  q-item-section
+                    div.q-gutter-xs
+                      Field(name="keysounded" v-slot="{ field }")
+                        q-btn(flat :label="$t('patterns.all')" :text-color="field.value === '' ? 'tech' : 'grey'" @click="field.onChange('')")
+                        q-btn(flat :label="$t('patterns.yes')" :text-color="field.value === 'true' ? 'tech' : 'grey'" @click="field.onChange('true')")
+                        q-btn(flat :label="$t('patterns.no')" :text-color="field.value === 'false' ? 'tech' : 'grey'" @click="field.onChange('false')")
+                //- Controls
+                q-item
+                  q-item-section {{ $t('submitForm.control') }}
+                  q-item-section
+                    div.q-gutter-xs
+                      template(v-for="(controlOption) in controlOptions" :key="controlOption")
+                        Field(name="controls" v-slot="{ field }" type="checkbox" :value="controlOption")
+                          q-checkbox(
+                            keep-color color="tech"
+                            :name="`controls`+controlOption"
+                            :model-value="field.checked"
+                            @update:model-value="field.onInput($event)"
+                            :label="$t('pattern.'+controls[controlOption])"
+                          )
+                //- Lanes
+                q-item
+                  q-item-section {{ $t('submitForm.lanes') }}
+                  q-item-section
+                    div.q-gutter-xs
+                      template(v-for="(lanesOption) in lanesOptions" :key="lanesOption")
+                        Field(name="lanes" v-slot="{ field }" type="checkbox" :value="lanesOption")
+                          q-checkbox(
+                            keep-color color="tech"
+                            :name="`lanes`+lanesOption"
+                            :model-value="field.checked"
+                            @update:model-value="field.onInput($event)"
+                            :label="lanesOption + `L`"
+                          )
+                //- Sort
+                q-item
+                  q-item-section {{ $t('patterns.sort') }}
+                  q-item-section
+                    div.q-gutter-xs
+                      Field(name="sortBy" v-slot="{ field }")
+                        q-btn(flat :label="$t('patterns.sortSubmit')" :icon-right="getSortIcon('createdAt')" :text-color="field.value === 'createdAt' ? 'tech' : 'grey'" @click="changeSort('createdAt')")
+                        q-btn(flat :label="$t('patterns.sortUpdate')" :icon-right="getSortIcon('updatedAt')" :text-color="field.value === 'updatedAt' ? 'tech' : 'grey'" @click="changeSort('updatedAt')")
+                        q-btn(flat :label="$t('patterns.sortName')" :icon-right="getSortIcon('name')" :text-color="field.value === 'name' ? 'tech' : 'grey'" @click="changeSort('name')")
+                        q-btn(flat :label="$t('pattern.rating')" :icon-right="getSortIcon('rating')" :text-color="field.value === 'rating' ? 'tech' : 'grey'" @click="changeSort('rating')")
+          //- Patterns
           q-separator.q-my-md
           q-infinite-scroll.row.q-my-md(@load="loadScroll" :offset="200" :disable="scrollDisable")
-            .col-xs-12.col-sm-6.col-lg-3.q-pa-md.q-my-xs(v-for="pattern in patterns" :key="pattern.id")
+            .col-xs-12.col-sm-6.col-md-4.col-lg-3.q-pa-md.q-my-xs(v-for="pattern in patterns" :key="pattern.id")
               PatternCard(:pattern="pattern" :mine="false")
             template(#loading)
               q-spinner-dots(color="tech" size="40px")
           .text-center.text-body1(v-if="patterns.length === 0 && scrollDisable") {{ $t('patterns.notFound') }}
-  q-no-ssr
     q-page-sticky(position="bottom-right" :offset="[36,36]" v-if="user.isLogin")
       q-btn(fab icon="add" color="tech" text-color="black" @click="$router.push('/patterns/new')")
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, useTemplateRef, onMounted } from 'vue'
 import { useMeta } from 'quasar'
 import { useRoute } from 'vue-router'
+import * as yup from 'yup'
+import { Form, Field } from 'vee-validate'
 import { useUserStore } from 'src/stores/user'
 import PatternCard from 'src/components/PatternCard.vue'
 import api from 'src/utils/api'
+import { controls, CONTROL_TOUCH, CONTROL_KEYS, CONTROL_KM } from 'src/utils/control'
 
 const route = useRoute()
 const user = useUserStore()
@@ -125,22 +164,41 @@ const metaData = {
 useMeta(metaData)
 
 const patterns = ref([])
-const searchForm = ref({
-  keywords: '',
-  keysounded: -1,
-  control: '',
-  sort: -1,
-  lanes: [2, 3, 4],
-  sortBy: 'createdAt',
+
+// // Search form schema lanes select options
+const lanesOptions = [2, 3, 4]
+const controlOptions = [CONTROL_TOUCH, CONTROL_KEYS, CONTROL_KM]
+// Search form template ref
+const form = useTemplateRef('form')
+// Search form validation schema
+const schema = yup.object({
+  keywords: yup.string(),
+  keysounded: yup.string().oneOf(['', 'true', 'false']),
+  controls: yup.array().of(yup.number().oneOf([CONTROL_TOUCH, CONTROL_KEYS, CONTROL_KM])),
+  lanes: yup.array().of(yup.number().min(2).max(4)),
+  sort: yup.number().required().min(-1).max(1),
+  sortBy: yup.string().required().oneOf(['createdAt', 'updatedAt', 'name', 'rating']),
 })
+// Search form initial values
+const initialValues = {
+  keywords: '',
+  keysounded: '',
+  controls: [CONTROL_TOUCH, CONTROL_KEYS, CONTROL_KM],
+  lanes: [2, 3, 4],
+  sort: -1,
+  sortBy: 'createdAt',
+}
+
+// Current search filters for API request
 const search = ref({
   keywords: '',
-  keysounded: -1,
-  control: '',
-  sort: -1,
+  keysounded: '',
+  controls: [CONTROL_TOUCH, CONTROL_KEYS, CONTROL_KM],
   lanes: [2, 3, 4],
+  sort: -1,
   sortBy: 'createdAt',
 })
+// Infinite scroll disable flag
 const scrollDisable = ref(false)
 
 /**
@@ -149,17 +207,22 @@ const scrollDisable = ref(false)
  */
 const fetchPatterns = async (start = 0) => {
   try {
-    const { data } = await api.get(
-      `/patterns?start=${start}&keysounded=${search.value.keysounded}&control=${search.value.control}&keywords=${search.value.keywords}&lanes=${search.value.lanes.join()}&sort=${search.value.sort}&sortBy=${search.value.sortBy}&limit=12`,
-    )
-    if (data.success) {
-      if (data.result.length > 0) patterns.value = patterns.value.concat(data.result)
-      else scrollDisable.value = true
-    } else {
-      throw new Error('Error')
-    }
+    const { data } = await api.get('/patterns', {
+      params: {
+        start,
+        keysounded: search.value.keysounded,
+        controls: search.value.controls.join(),
+        keywords: search.value.keywords,
+        lanes: search.value.lanes.join(),
+        sort: search.value.sort,
+        sortBy: search.value.sortBy,
+        limit: 12,
+      },
+    })
+    if (data.result.length > 0) patterns.value = patterns.value.concat(data.result)
+    else scrollDisable.value = true
   } catch (error) {
-    error.value = true
+    console.error(error)
     scrollDisable.value = true
   }
 }
@@ -175,17 +238,22 @@ const loadScroll = async (index, done) => {
 }
 
 /**
- * Apply search filters
+ * On search form submit, apply search filters
  */
-const applySearch = () => {
+const onSearchSubmit = async (values) => {
+  // Reset patterns
   patterns.value = []
-  search.value.keysounded = searchForm.value.keysounded
-  search.value.control = searchForm.value.control
-  search.value.lanes = searchForm.value.lanes
-  search.value.sort = searchForm.value.sort
-  search.value.sortBy = searchForm.value.sortBy
+  // Apply search filters
+  search.value.keywords = values.keywords
+  search.value.keysounded = values.keysounded
+  search.value.controls = values.controls
+  search.value.lanes = values.lanes
+  search.value.sort = values.sort
+  search.value.sortBy = values.sortBy
+  // Reset infinite scroll disable flag
   scrollDisable.value = false
-  fetchPatterns()
+  // Fetch patterns
+  await fetchPatterns()
 }
 
 /**
@@ -194,8 +262,9 @@ const applySearch = () => {
  * @returns The icon name
  */
 const getSortIcon = (sortBy) => {
-  if (searchForm.value.sortBy === sortBy)
-    return searchForm.value.sort > 0 ? 'arrow_drop_up' : 'arrow_drop_down'
+  if (!form.value) return undefined
+  if (form.value.values.sortBy === sortBy)
+    return form.value.values.sort > 0 ? 'arrow_drop_up' : 'arrow_drop_down'
   else return undefined
 }
 
@@ -204,10 +273,12 @@ const getSortIcon = (sortBy) => {
  * @param sortBy - The sort by field
  */
 const changeSort = (sortBy) => {
-  if (searchForm.value.sortBy === sortBy) searchForm.value.sort *= -1
+  const currentSortBy = form.value.values.sortBy
+  const currentSort = form.value.values.sort
+  if (currentSortBy === sortBy) form.value.setFieldValue('sort', currentSort * -1)
   else {
-    searchForm.value.sortBy = sortBy
-    searchForm.value.sort = -1
+    form.value.setFieldValue('sort', -1)
+    form.value.setFieldValue('sortBy', sortBy)
   }
 }
 
