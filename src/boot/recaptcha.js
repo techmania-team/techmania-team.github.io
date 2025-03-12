d60e4f886b50ac0ab9914ce39798fb5151b115f0
Kento <rogeraabbccdd@gmail.com>
2025-03-12 23:34:03 +0800

    Update recaptcha boot file to show/hide badge based on route meta

--- src/boot/recaptcha.js
+++ src/boot/recaptcha.js
@@ -1,6 +1,25 @@
 import { defineBoot } from '#q-app/wrappers'
-import { VueReCaptcha } from 'vue-recaptcha-v3'
+import { VueReCaptcha, useReCaptcha } from 'vue-recaptcha-v3'
 
-export default defineBoot(({ app }) => {
-  app.use(VueReCaptcha, { siteKey: process.env.RECAPTCHA_SITE_KEY, loaderOptions: { autoHideBadge: true } })
+export default defineBoot(({ app, router }) => {
+  app.use(VueReCaptcha, {
+    siteKey: process.env.RECAPTCHA_SITE_KEY,
+    loaderOptions: { autoHideBadge: true },
+  })
+
+  router.afterEach(async (to) => {
+    if (!process.env.CLIENT) return
+
+    const recaptcha = useReCaptcha()
+
+    // Wait for recaptcha to be loaded
+    await recaptcha.recaptchaLoaded()
+
+    // Show or hide the badge based on the route meta
+    if (to.meta.recaptcha) {
+      recaptcha.instance.value.showBadge()
+    } else {
+      recaptcha.instance.value.hideBadge()
+    }
+  })
 })
