03bcefee3365333c6b25a6b45b16f5926cf019b4
Kento <rogeraabbccdd@gmail.com>
2025-03-13 02:49:19 +0800

    Optimize API checkImage function

--- src-ssr/api/utils/image.js
+++ src-ssr/api/utils/image.js
@@ -4,7 +4,7 @@ import handleError from './handleError'
 export const checkImage = async (link) => {
   let valid = false
   try {
-    const response = await axios.get(new URL(link).href)
+    const response = await axios.get(link)
     if (response.headers['content-type'].includes('image')) {
       valid = true
     }
