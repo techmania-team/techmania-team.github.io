dfef1717377708066d9aea8046e27acd4f681399
Kento <rogeraabbccdd@gmail.com>
2025-03-18 22:09:27 +0800

    Improve user API

--- src-ssr/api/controllers/users.js
+++ src-ssr/api/controllers/users.js
@@ -1,13 +1,43 @@
 import mongoose from 'mongoose'
+import * as yup from 'yup'
+import validator from 'validator'
 import users from '../models/users'
-import comments from '../models/comments'
 
 export const getById = async (req, res) => {
   try {
+    // Request params validation schema
+    const paramsSchema = yup.object({
+      id: yup
+        .string()
+        .required()
+        .test('mongoID', 'Invalid ID', (value) => {
+          return validator.isMongoId(value)
+        }),
+    })
+    // Parsed request params
+    const parsedParams = await paramsSchema.validate(req.params, { stripUnknown: true })
+
     const result = await users.aggregate([
       {
         $match: {
-          _id: mongoose.Types.ObjectId(req.params.id),
+          _id: new mongoose.Types.ObjectId(parsedParams.id),
+        },
+      },
+      {
+        $addFields: {
+          avatar: {
+            $concat: [
+              'https://cdn.discordapp.com/avatars/',
+              {
+                $toString: '$discord',
+              },
+              '/',
+              {
+                $toString: '$avatar',
+              },
+              '.png',
+            ],
+          },
         },
       },
       {
@@ -34,12 +64,23 @@ export const getById = async (req, res) => {
         },
       },
       {
+        $lookup: {
+          from: 'comments',
+          localField: '_id',
+          foreignField: 'replies.user',
+          as: 'comments',
+        },
+      },
+      {
         $addFields: {
           patternCount: {
             $size: '$patterns',
           },
           skinCount: {
             $size: '$skins',
+          },
+          replyCount: {
+            $size: '$comments',
           },
         },
       },
@@ -48,25 +89,14 @@ export const getById = async (req, res) => {
           patterns: 0,
           skins: 0,
           discord: 0,
+          comments: 0,
         },
       },
     ])
-    // some old users do not have this
-    if (!result[0].avatar) {
-      result[0].avatar =
-        'https://raw.githubusercontent.com/techmania-team/techmania-team.github.io/master/public/assets/Logo_black.png'
-    }
-    const result2 = await comments.aggregate([
-      {
-        $match: {
-          'replies.0.user': mongoose.Types.ObjectId(req.params.id),
-        },
-      },
-      {
-        $count: 'replyCount',
-      },
-    ])
-    res.status(200).send({ success: true, message: '', result: { ...result[0], ...result2[0] } })
+
+    // Only return the first result
+    // Because we only find 1 user by ID
+    res.status(200).send({ success: true, message: '', result: result[0] })
   } catch (error) {
     if (error.name === 'CastError') {
       res.status(404).send({ success: false, message: 'Not found' })
