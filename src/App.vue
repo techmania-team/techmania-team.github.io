391a216743ae282323501d8cbeb05916f530e230
Kento <rogeraabbccdd@gmail.com>
2025-03-19 19:04:19 +0800

    Move i18n locale handling from App.vue to i18n boot

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,4 @@
 router-view
 </template>
 
-<script setup>
-import { onMounted } from 'vue'
-import { useI18n } from 'vue-i18n'
-import { useQuasar } from 'quasar'
-import { useSettingsStore } from 'src/stores/settings'
-import { localeOptions } from './i18n'
-
-const i18n = useI18n()
-const $q = useQuasar()
-const settings = useSettingsStore()
-
-onMounted(async () => {
-  if (process.env.CLIENT) {
-    if (settings.locale) {
-      if (!localeOptions.includes(settings.locale)) {
-        settings.locale = $q.lang.getLocale()
-      }
-      i18n.locale.value = settings.locale
-    } else {
-      const localeDetected = $q.lang.getLocale()
-      settings.locale = localeDetected
-      i18n.locale.value = localeDetected
-    }
-  }
-})
-</script>
+<script setup></script>
--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -1,16 +1,32 @@
 import { defineBoot } from '#q-app/wrappers'
+import { Lang } from 'quasar'
 import { createI18n } from 'vue-i18n'
-import messages from 'src/i18n'
+import messages, { localeOptions } from 'src/i18n'
+import { useSettingsStore } from 'src/stores/settings'
 
 export default defineBoot(({ app }) => {
   const i18n = createI18n({
     locale: 'en-US',
     fallbackLocale: 'en-US',
     messages,
-    silentFallbackWarn: true
+    silentFallbackWarn: true,
   })
 
   // Set i18n instance on app
   app.use(i18n)
-})
 
+  if (process.env.CLIENT) {
+    const settings = useSettingsStore()
+
+    if (settings.locale) {
+      if (!localeOptions.includes(settings.locale)) {
+        settings.locale = Lang.getLocale()
+      }
+      i18n.global.locale.value = settings.locale
+    } else {
+      const localeDetected = Lang.getLocale()
+      settings.locale = localeDetected
+      i18n.global.locale.value = localeDetected
+    }
+  }
+})
