3f4e5a2294833ab2019021937318086d2b34e7d0
Kento <rogeraabbccdd@gmail.com>
2025-03-19 18:49:26 +0800

    Fix persist store boot function
    This make pattern and skin page empty after refresh

--- src/boot/persist-store.js
+++ src/boot/persist-store.js
@@ -13,15 +13,12 @@ function syncState(state, store) {
 // https://github.com/TobyMosque/quasar-v2-ssr-pinia/blob/master/src/boot/persist-store.ts
 export default defineBoot(({ store }) => {
   store.use(({ store, options }) => {
+    if (process.env.SERVER) return
+
     if (options.persist) {
       const name = options.persist.key || store.$id
       let state = null
-
-      // the state is being persisted at the Local/Session Storage (client only)
-      if (process.env.SERVER) return
-
       state = LocalStorage.getItem(name)
-
       syncState(state, store)
       store.$subscribe((_, state) => {
         LocalStorage.set(name, state)
