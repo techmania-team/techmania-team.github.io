a2aa89d54ce49fa8f8cd00a11db840c87552d181
Kento <rogeraabbccdd@gmail.com>
2025-03-28 14:07:25 +0800

    Add Setlists model

---
+++ src/utils/criteria.js
@@ -0,0 +1,13 @@
+export const criterias = ['none', 'index', 'level', 'hp', 'score', 'combo', 'maxCombo', 'd100']
+
+export const CRITERIA_NONE = 0
+export const CRITERIA_INDEX = 1
+export const CRITERIA_LEVEL = 2
+export const CRITERIA_HP = 3
+export const CRITERIA_SCORE = 4
+export const CRITERIA_COMBO = 5
+export const CRITERIA_MAX_COMBO = 6
+export const CRITERIA_D100 = 7
+
+export const CRITERIA_DIRECTION_LOWER = 0
+export const CRITERIA_DIRECTION_GREATER = 1
