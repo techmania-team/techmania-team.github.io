24d1e4e8de2909b3c7d07531904c9d48ff9f0e1a
Kento <rogeraabbccdd@gmail.com>
2025-05-18 11:24:16 +0800

    Fix Discord guild check

--- src-ssr/api/middleware/guild.js
+++ src-ssr/api/middleware/guild.js
@@ -1,14 +1,27 @@
 import { REST, Routes } from 'discord.js'
 import handleServerError from '../utils/handleServerError'
+import { refreshAccessToken } from '../utils/discord'
+
+const checkInGuild = async (accessToken) => {
+  const rest = new REST({ version: '10' }).setToken(accessToken)
+  const guilds = await rest.get(Routes.userGuilds(), { authPrefix: 'Bearer' })
+  return guilds.some((guild) => guild.id.toString() === process.env.DISCORD_GUILD.toString())
+}
 
 export default async (req, res, next) => {
   try {
-    // check in discord guild or not
-    const rest = new REST({ version: '10' }).setToken(req.user.accessToken)
-    const guilds = await rest.get(Routes.userGuilds(), { authPrefix: 'Bearer' })
-    const inGuild = guilds.some(
-      (guild) => guild.id.toString() === process.env.DISCORD_GUILD.toString(),
-    )
+    let accessToken = req.user.accessToken
+    let inGuild
+    try {
+      inGuild = await checkInGuild(accessToken)
+    } catch (error) {
+      if (req.user.refreshToken) {
+        await refreshAccessToken(req)
+        inGuild = await checkInGuild(accessToken)
+      } else {
+        throw error
+      }
+    }
     if (!inGuild) {
       res.status(403).send({ success: false, message: 'Not in guild' })
     } else {
---
+++ src-ssr/api/utils/discord.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import User from '../models/users'
+
+export const refreshAccessToken = async (req) => {
+  const params = new URLSearchParams()
+  params.append('client_id', process.env.DISCORD_CLIENT)
+  params.append('client_secret', process.env.DISCORD_SECRET)
+  params.append('grant_type', 'refresh_token')
+  params.append('refresh_token', req.user.refreshToken)
+  try {
+    const { data } = await axios.post('https://discord.com/api/oauth2/token', params, {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    })
+    req.user.accessToken = data.access_token
+    req.user.refreshToken = data.refresh_token
+    await User.findByIdAndUpdate(req.user._id, {
+      discordToken: data.access_token,
+      discordRefreshToken: data.refresh_token,
+    })
+  } catch {
+    throw new Error('Failed to refresh access token')
+  }
+}
