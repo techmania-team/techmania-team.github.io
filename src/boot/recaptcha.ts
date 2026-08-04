e1af9e508691f0d31703c1c2bb358a90ce71d8d3
Kento <rogeraabbccdd@gmail.com>
2026-08-04 19:08:17 +0800

    Migrate recaptcha to TS

--- src/boot/recaptcha.ts
+++ src/boot/recaptcha.ts
@@ -1,28 +1,28 @@
-import { defineBoot } from '#q-app/wrappers'
-import { VueReCaptcha, useReCaptcha } from 'vue-recaptcha-v3'
-import { useUserStore } from 'src/stores/user'
+import { useReCaptcha, VueReCaptcha } from 'vue-recaptcha-v3'
+import { defineBoot } from '#q-app'
+import { useUserStore } from '@/stores/user'
 
 export default defineBoot(({ app, router }) => {
   app.use(VueReCaptcha, {
-    siteKey: process.env.RECAPTCHA_SITE_KEY,
+    siteKey: import.meta.env.QCLI_RECAPTCHA_SITE_KEY,
     loaderOptions: { autoHideBadge: true },
   })
 
   router.afterEach(async (to) => {
-    if (!process.env.CLIENT) return
+    if (!import.meta.env.QUASAR_CLIENT) return
 
     const recaptcha = useReCaptcha()
     const user = useUserStore()
 
     // Wait for recaptcha to be loaded
-    await recaptcha.recaptchaLoaded()
+    await recaptcha?.recaptchaLoaded()
 
     // Show or hide the badge based on the route meta
     // Recaptcha only applies to submission pages and comment sections, which require login
     if (to.meta.recaptcha && user.isLogin) {
-      recaptcha.instance.value.showBadge()
+      recaptcha?.instance.value?.showBadge()
     } else {
-      recaptcha.instance.value.hideBadge()
+      recaptcha?.instance.value?.hideBadge()
     }
   })
 })
