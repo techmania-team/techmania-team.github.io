f4f32ca137f4d868ba4ad29a39e6b8064f42e106
Kento <rogeraabbccdd@gmail.com>
2026-08-05 09:22:18 +0800

    Fix date hydration error

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,4 @@
-import { formatDistanceToNow } from 'date-fns'
+import { format, formatDistanceToNow } from 'date-fns'
 import { parseISO } from 'date-fns/fp/parseISO'
 import { enUS, ja, ko, zhCN, zhTW } from 'date-fns/locale'
 import { useI18n } from 'vue-i18n'
@@ -26,5 +26,10 @@ export const toRelative = (date: string) => {
 export const toLocaleString = (date: string) => {
   const i18n = useI18n()
   if (!date || !i18n) return ''
-  return new Date(date).toLocaleString(i18n.locale.value)
+
+  const currentLocale = i18n.locale.value as keyof typeof locales
+
+  return format(parseISO(date), 'yyyy/MM/dd aa hh:mm:ss', {
+    locale: locales[currentLocale],
+  })
 }
