d066290bde592412ee8e21292c0e4fe142b687e7
Kento <rogeraabbccdd@gmail.com>
2026-08-20 10:17:55 +0800

    Migrate to vue-gtag

--- src/boot/analytics.js
+++
@@ -1,8 +0,0 @@
-import { defineRouter } from '#q-app/wrappers'
-import ga from 'src/utils/analytics'
-
-export default defineRouter(({ router }) => {
-  router.afterEach((to) => {
-    ga.logPage(to.path, to.name)
-  })
-})
