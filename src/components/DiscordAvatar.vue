a350baa23f57e73db272dc7a8cfea3942da25d1d
Kento <rogeraabbccdd@gmail.com>
2026-08-31 18:03:50 +0800

    Fix Discord avatar

--- src/components/DiscordAvatar.vue
+++ src/components/DiscordAvatar.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 q-avatar(v-bind="avatarOptions")
-  q-img(:src="avatar" error-src="/assets/notes/basic.png" v-bind="imgOptions")
+  q-img(:src="avatar || DEFAULT_AVATAR" :error-src="DEFAULT_AVATAR" v-bind="imgOptions")
 </template>
 
 <script setup lang="ts">
@@ -10,4 +10,6 @@ defineProps<{
   avatarOptions?: QAvatarProps
   imgOptions?: QImgProps
 }>()
+
+const DEFAULT_AVATAR = '/assets/notes/basic.png'
 </script>
