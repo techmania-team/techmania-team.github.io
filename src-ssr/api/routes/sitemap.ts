f454eea599e7a695aff41dd6184316a261802242
Kento <rogeraabbccdd@gmail.com>
2026-09-01 12:22:14 +0800

    Fix lint

--- src-ssr/api/routes/sitemap.ts
+++ src-ssr/api/routes/sitemap.ts
@@ -70,7 +70,13 @@ router.get('/', async (_req, res) => {
       buildUrls(`/users/${u._id.toString()}/setlists`),
     ])
 
-    const allUrls = [...staticPages, ...patternPages, ...skinPages, ...setlistPages, ...userPages].join('\n')
+    const allUrls = [
+      ...staticPages,
+      ...patternPages,
+      ...skinPages,
+      ...setlistPages,
+      ...userPages,
+    ].join('\n')
 
     const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
